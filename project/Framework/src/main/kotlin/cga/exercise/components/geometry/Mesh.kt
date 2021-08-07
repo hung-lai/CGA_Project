06565a85ae0ea45d424bf85075fb1a52bd5ffbbc
hung-lai <88457121+hung-lai@users.noreply.github.com>
2021-08-07 20:26:10 +0200

    Add files via upload

---
+++ project/Framework/src/main/kotlin/cga/exercise/components/geometry/Mesh.kt
@@ -0,0 +1,49 @@
+package cga.exercise.components.geometry
+
+import org.lwjgl.opengl.GL11
+import org.lwjgl.opengl.GL15
+import org.lwjgl.opengl.GL20
+import org.lwjgl.opengl.GL30
+
+/**
+ * Creates a Mesh object from vertexdata, intexdata and a given set of vertex attributes
+ *
+ * @param vertexdata plain float array of vertex data
+ * @param indexdata  index data
+ * @param attributes vertex attributes contained in vertex data
+ * @throws Exception If the creation of the required OpenGL objects fails, an exception is thrown
+ *
+ * Created by Fabian on 16.09.2017.
+ */
+class Mesh(vertexdata: FloatArray, indexdata: IntArray, attributes: Array<VertexAttribute>) {
+    //private data
+    private var vao = 0
+    private var vbo = 0
+    private var ibo = 0
+    private var indexcount = 0
+
+    init {
+        // todo: place your code here
+
+        // todo: generate IDs
+        // todo: bind your objects
+        // todo: upload your mesh data
+    }
+
+    /**
+     * renders the mesh
+     */
+    fun render() {
+        // todo: place your code here
+        // call the rendering method every frame
+    }
+
+    /**
+     * Deletes the previously allocated OpenGL objects for this mesh
+     */
+    fun cleanup() {
+        if (ibo != 0) GL15.glDeleteBuffers(ibo)
+        if (vbo != 0) GL15.glDeleteBuffers(vbo)
+        if (vao != 0) GL30.glDeleteVertexArrays(vao)
+    }
+}
---
+++ project/Framework/src/main/kotlin/cga/exercise/game/Scene.kt
@@ -0,0 +1,41 @@
+package cga.exercise.game
+
+import cga.exercise.components.shader.ShaderProgram
+import cga.framework.GLError
+import cga.framework.GameWindow
+import org.lwjgl.opengl.GL11.*
+
+
+/**
+ * Created by Fabian on 16.09.2017.
+ */
+class Scene(private val window: GameWindow) {
+    private val staticShader: ShaderProgram
+
+
+    //scene setup
+    init {
+        staticShader = ShaderProgram("assets/shaders/simple_vert.glsl", "assets/shaders/simple_frag.glsl")
+
+        //initial opengl state
+        glClearColor(0.6f, 1.0f, 1.0f, 1.0f); GLError.checkThrow()
+        glDisable(GL_CULL_FACE); GLError.checkThrow()
+        //glFrontFace(GL_CCW); GLError.checkThrow()
+        //glCullFace(GL_BACK); GLError.checkThrow()
+        glEnable(GL_DEPTH_TEST); GLError.checkThrow()
+        glDepthFunc(GL_LESS); GLError.checkThrow()
+    }
+
+    fun render(dt: Float, t: Float) {
+        glClear(GL_COLOR_BUFFER_BIT or GL_DEPTH_BUFFER_BIT)
+    }
+
+    fun update(dt: Float, t: Float) {}
+
+    fun onKey(key: Int, scancode: Int, action: Int, mode: Int) {}
+
+    fun onMouseMove(xpos: Double, ypos: Double) {}
+
+
+    fun cleanup() {}
+}
