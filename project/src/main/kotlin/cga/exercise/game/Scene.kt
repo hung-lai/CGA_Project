cb878411c838d2562711d555c8067df3411cd0d3
alanu <alan.umarow@smail.th-koeln.de>
2021-08-22 23:20:52 +0200

    Spotlight - Straßenlichter1

--- project/src/main/kotlin/cga/exercise/game/Scene.kt
+++ project/src/main/kotlin/cga/exercise/game/Scene.kt
@@ -163,7 +163,6 @@ public class Scene(private val window: GameWindow) {
         spotLightCar2BL = SpotLight(Vector3f(-1.2f, 1.0f,1.0f), Vector3f(1.0f,0.0f,0.0f))
 
         car1.scaleLocal(Vector3f(0.8f))
-        car1.translateLocal(Vector3f(-110.0f,1.002f, -40.45f))
         car1.translateLocal(Vector3f(-117.0f,1.002f, -45.45f))
 
         car2.scaleLocal(Vector3f(0.8f))
